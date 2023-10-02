35e10c5f7070d23525242f63e3284e5ad54f65fb
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-10-02 15:41:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Plantilla2D
+Plantilla base para proyectos en 2D con P5.JS
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+  // put preload code here
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(800,800)
+}
+
+function draw() {
+  // put drawing code here
+  background(255)
+  rect(100,100,200,200)
+}
