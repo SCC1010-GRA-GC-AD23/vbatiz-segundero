837856f751eff0996d48d1ef31e21c5e77441a6a
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-10-03 23:01:19 +0000

    Puse el texto fijo

--- sketch.js
+++ sketch.js
@@ -18,19 +18,21 @@ function draw() {
   // put drawing code here
   background(255)
   //rect(100,100,200,200)
+  
   translate(width/2,height/2)
   h = nf(hour(),2)
   m = nf(minute(),2)
   s = nf(second(),2)
   textAlign(CENTER,CENTER)
   textSize(32)
-  
+  push()
   rot = s * (6 * grado)
   rotate(rot)
   
   circle(0,0,200)
   strokeWeight(2)
   line(0,0,0,-100)
+  pop()
   text(h+":"+m+":"+s,0,0)
   strokeWeight(1)
 }
